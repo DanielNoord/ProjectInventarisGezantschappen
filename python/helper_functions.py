f122661de685390ebceb2364f1730deec0904be0
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-02-10 23:28:28 +0100

    Improved handling of dates without specific day

--- python/helper_functions.py
+++ python/helper_functions.py
@@ -2,23 +2,6 @@ import re
 import locale
 import datetime
 
-class Mdate(datetime.date):
-    """ A datetime.date where day doesn't matter. """
-
-    def __new__(cls, year, month):
-        """Python calls __new__ instead of __init__ for immutable objects."""
-        return super().__new__(cls, year, month, 1)
-
-    def __repr__(self):
-        """Because you're implementing __new__ you also need __repr__ or else you get
-        TypeError: __new__() takes 3 positional arguments but 4 were given."""
-        return '{0}({1}, {2}, 1)'.format(self.__class__.__name__, self.year, self.month)
-
-    def __reduce__(self):
-        """You need __reduce__ to support pickling."""
-        return (self.__class__, (self.year, self.month))
-
-
 def extract_date(date_string, localization):
     locale.setlocale(locale.LC_ALL, localization)
     date_pattern = re.compile(r"(\w{4})?-?(\w{2})?-?(\w{2})?/?(\w{4})?-?(\w{2})?-?(\w{2})?$")
@@ -28,7 +11,7 @@ def extract_date(date_string, localization):
             if d_1:
                 date1 = datetime.date(int(y_1), int(m_1), int(d_1)).strftime("%d %B %Y")
             else:
-                date1 = Mdate(int(y_1), int(m_1)).strftime("%B %Y")
+                date1 = datetime.date(int(y_1), int(m_1), 1).strftime("%B %Y")
         else:
             date1 = y_1
     else:
@@ -38,7 +21,7 @@ def extract_date(date_string, localization):
             if d_2:
                 date2 = datetime.date(int(y_2), int(m_2), int(d_2)).strftime("%d %B %Y")
             else:
-                date2 = Mdate(int(y_2), int(m_2)).strftime("%B %Y")
+                date2 = datetime.date(int(y_2), int(m_2), 1).strftime("%B %Y")
         else:
             date2 = y_2
     else:
