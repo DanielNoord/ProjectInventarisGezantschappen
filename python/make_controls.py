b7051a8e234aac892cf1e0630f30af0cc6d70486
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-03-04 00:05:10 +0100

    Added possibility of comments for in JSON, Controls only consider "non-unsure" entries

--- python/make_controls.py
+++ python/make_controls.py
@@ -1,26 +1,37 @@
+import json
+
 from functions.create_docx import list_of_translated_data
-from functions.translate import initialize_translation_database
 
 
 def create_controle_transaltions():
     """Create .docx files of all titles and functions found by the translate functions
     Includes Italian and English translations on second and third row
     """
-    data = initialize_translation_database()
+    with open("inputs/Translations/Titles.json") as file1:
+        titles = json.load(file1)
+    del titles["$schema"]
+
+    with open("inputs/Translations/Functions.json") as file2:
+        functions = json.load(file2)
+    del functions["$schema"]
 
     translated_titles = []
     translated_functions = []
 
-    for title, translations in sorted(data[0].items()):
+    for title, translations in sorted(titles.items()):
         result = [title]
         result.append(translations["it_IT"])
         result.append(translations["en_GB"])
+        if translations.get("comment"):
+            result.append(f"_Opmerking_: {translations['comment']}")
         translated_titles.append(result)
 
-    for function, translations in sorted(data[1].items()):
+    for function, translations in sorted(functions.items()):
         result = [function]
         result.append(translations["it_IT"])
         result.append(translations["en_GB"])
+        if translations.get("comment"):
+            result.append(f"_Opmerking_: {translations['comment']}")
         translated_functions.append(result)
 
     list_of_translated_data(translated_titles, "Titles")
