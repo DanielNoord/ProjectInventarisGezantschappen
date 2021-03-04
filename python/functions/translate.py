ea625d1313e756293945d474ed11a26c69dc0e58
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-03-04 12:34:18 +0100

    Added way to track changes to translations

--- python/functions/translate.py
+++ python/functions/translate.py
@@ -9,16 +9,20 @@ def initialize_translation_database():
             Each entry (in "nl_NL") is a dictionary with the keys "nl_NL", "it_IT" and "en_GB"
             "nl_NL" is double but for ease of translation and reading the input-files in Word
     """
-    with open("inputs/Translations/Titles.json") as file1:
-        titles = json.load(file1)
-    with open("inputs/Translations/TitlesUnsure.json") as file2:
-        titles = dict(titles, **json.load(file2))
+    with open("inputs/Translations/Titles.json") as file:
+        titles = json.load(file)
+    with open("inputs/Translations/TitlesUnsure.json") as file:
+        titles = dict(titles, **json.load(file))
+    with open("inputs/Translations/TitlesChanged.json") as file:
+        titles = dict(titles, **json.load(file))
     del titles["$schema"]
 
-    with open("inputs/Translations/Functions.json") as file3:
-        functions = json.load(file3)
-    with open("inputs/Translations/FunctionsUnsure.json") as file4:
-        functions = dict(functions, **json.load(file4))
+    with open("inputs/Translations/Functions.json") as file:
+        functions = json.load(file)
+    with open("inputs/Translations/FunctionsUnsure.json") as file:
+        functions = dict(functions, **json.load(file))
+    with open("inputs/Translations/FunctionsChanged.json") as file:
+        functions = dict(functions, **json.load(file))
     del functions["$schema"]
 
     return [titles, functions]
