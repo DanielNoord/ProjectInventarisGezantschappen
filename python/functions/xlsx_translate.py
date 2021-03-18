f0e22387f893d385bff9bf8e3dd4e22bae9ce3e4
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-03-18 11:58:04 +0100

    Started automating inventory translations

---
+++ python/functions/xlsx_translate.py
@@ -0,0 +1,80 @@
+# pylint: disable=W0123
+import os
+import re
+
+from openpyxl import load_workbook
+from openpyxl.styles import Font
+
+
+def translate_xlsx(directory_name, file_name, localization, translations):
+    """Translate .xlsx file
+
+    Args:
+        directory_name (str): Directory of .xlsx file
+        file_name (str): Name of .xlsx file
+        localization (str): Localization string of target language
+        translations (dict): Translations of common document titles
+    """    
+    workbook = load_workbook(f"{directory_name}/{file_name}")
+    for row in workbook[workbook.sheetnames[0]].iter_rows():
+        if row[1].value is not None:
+            line = row[1].value
+            if (mat := re.match(r"Lettera (di|del) (\$\w*) (a|al) (\$\w*)$", line)) :
+                line = eval(
+                    translations["Lettera (di|del) (n1) (a|al) (n2)$"][localization]
+                )(mat.groups()[1], mat.groups()[3])
+            elif (mat := re.match(r"Lettera (di|del) (\$\w*)$", line)) :
+                line = eval(
+                    translations["Lettera (di|del) (n1)$"][localization])(mat.groups()[1]
+                )
+            elif (mat := re.match(r"Lettera circolare (di|del) (\$\w*) (a|al) (\$\w*)$", line)):
+                line = eval(
+                    translations["Lettera circolare (di|del) (n1) (a|al) (n2)$"][localization]
+                )(mat.groups()[1], mat.groups()[3])
+            elif (mat := re.match(r"Lettera circolare (di|del) (\$\w*)$", line)) :
+                line = eval(
+                    translations["Lettera circolare (di|del) (n1)$"][localization]
+                )(mat.groups()[1])
+            elif (mat := re.match(r"Minuta di lettera (a|al) (\$\w*)$", line)) :
+                line = eval(
+                    translations["Minuta di lettera (a|al) (n1)$"][localization]
+                )(mat.groups()[1])
+            elif (mat := re.match(r"Minuta di lettera inviata (a|al) (\$\w*)$", line)) :
+                line = eval(
+                    translations["Minuta di lettera inviata (a|al) (n1)$"][localization]
+                )(mat.groups()[1])
+            elif (mat := re.match(r"Minuta di lettera (di|del) (\$\w*)$", line)) :
+                line = eval(
+                    translations["Minuta di lettera (di|del) (n1)$"][localization]
+                )(mat.groups()[1])
+            elif (mat := re.match(r"Minuta di lettera (di|del) (\$\w*) (a|al) (\$\w*)$", line)) :
+                line = eval(
+                    translations["Minuta di lettera (di|del) (n1) (a|al) (n2)$"][localization]
+                )(mat.groups()[1], mat.groups()[3])
+            elif re.match(r"Minuta di lettera$", line):
+                line = translations["Minuta di lettera$"][localization]
+            elif re.match(r"Minuta di risposta$", line):
+                line = translations["Minuta di risposta$"][localization]
+            elif re.match(r"Nota di ricezione dei dispacci diplomatici$", line):
+                line = translations["Nota di ricezione dei dispacci diplomatici$"][localization]
+            elif re.match(r"Bianca$", line):
+                line = translations["Bianca$"][localization]
+            else:
+                row[1].font = Font(color="FF0000")
+
+            # Clean up string
+            if line[-1] in [" ", ",", "."]:  # Remove final characters
+                line = line[:-1]
+            line = line[0].upper() + line[1:]
+            line = line.replace("( ", "(").replace(" )", ")")
+            row[1].value = line
+
+    new_directory = directory_name.replace("inputs", "outputs").replace(
+        "it_IT", f"translated_{localization}"
+    )
+    os.makedirs(
+        os.path.join(os.getcwd(), new_directory),
+        exist_ok=True,
+    )
+    workbook.save(f"{new_directory}/{file_name.replace('it_IT', localization)}")
+    print(f"File written to {new_directory}/{file_name.replace('it_IT', localization)}")
--- python/make_xlsx.py
+++ python/make_xlsx.py
@@ -1,13 +1,15 @@
 import json
 import os
+import re
 
-from openpyxl import load_workbook
+from openpyxl import Workbook, load_workbook
 
 from functions.create_name_string import name_string
 from functions.translate import initialize_translation_database
+from functions.xlsx_translate import translate_xlsx
 
 
-def translate_xlsx(directory_name, file_name, individuals, translations, localization):
+def fill_in_xlsx(directory_name, file_name, individuals, translations, localization):
     """Translates and writes an individual volume .xlsx file
 
     Args:
@@ -21,23 +23,32 @@ def translate_xlsx(directory_name, file_name, individuals, translations, localiz
     for row in workbook[workbook.sheetnames[0]].iter_rows():
         if row[1].value is not None:
             date = [row[2].value, row[3].value, row[4].value]
-            line = row[1].value.split()
+            line = re.split(r"( |\.|,|\(|\))", row[1].value)
             for index, word in enumerate(line):
                 if word.startswith("$"):
                     line[index] = name_string(
                         individuals[word], date, translations, localization
                     )
-            row[1].value = " ".join(line)
+            line = "".join(line)
+
+            # Clean up string
+            if line[-1] in [" ", ",", "."]:  # Remove final characters
+                line = line[:-1]
+            line = line[0].upper() + line[1:]
+            line = line.replace("( ", "(").replace(" )", ")")
+            row[1].value = line
 
     os.makedirs(
         os.path.join(os.getcwd(), directory_name.replace("inputs", "outputs")),
         exist_ok=True,
     )
-    workbook.save(f"{directory_name.replace('inputs', 'outputs')}/{file_name}")
-    print(f"File written to {directory_name.replace('inputs', 'outputs')}/{file_name}")
+    workbook.save(f"{directory_name.replace('inputs', 'outputs')}/Modified_{file_name}")
+    print(
+        f"File written to {directory_name.replace('inputs', 'outputs')}/Modified_{file_name}"
+    )
 
 
-def create_xlsx(directory_name, localization):
+def create_filled_xlsx(directory_name, localization):
     """Creates the .xlsx files of all volumes in the input directory
 
     Args:
@@ -53,7 +64,7 @@ def create_xlsx(directory_name, localization):
     for file in os.listdir(directory):
         if not str(file).count("~$") and str(file).startswith("b'Paesi"):
             filename = os.fsdecode(file)
-            translate_xlsx(
+            fill_in_xlsx(
                 directory_name,
                 filename,
                 individuals_data,
@@ -62,7 +73,85 @@ def create_xlsx(directory_name, localization):
             )
 
 
+def create_xlsx_controle(directory_names):
+    """Creates a combination of the various translated .xlsx files into one "control file"
+
+    Args:
+        directory_names (list): Directories of the file to be merged
+    """
+    directory = os.fsencode(directory_names[0])
+    for file in os.listdir(directory):
+        if not str(file).count("~$") and str(file).startswith("b'Paesi"):
+            new_file = Workbook()
+            new_sheet = new_file.active
+            filename = os.fsdecode(file)
+            rows_original = [
+                i for i in load_workbook(f"{directory_names[0]}/{filename}").active.rows
+            ]
+            rows_it = [
+                i
+                for i in load_workbook(
+                    f"{directory_names[1]}/{filename.replace('.xlsx', '')}_it_IT.xlsx"
+                ).active.rows
+            ]
+            rows_en = [
+                i
+                for i in load_workbook(
+                    f"{directory_names[2]}/{filename.replace('.xlsx', '')}_en_GB.xlsx"
+                ).active.rows
+            ]
+            rows_nl = [
+                i
+                for i in load_workbook(
+                    f"{directory_names[3]}/{filename.replace('.xlsx', '')}_nl_NL.xlsx"
+                ).active.rows
+            ]
+            for index, row in enumerate(rows_original):
+                new_sheet.append(tuple(i.value for i in row))
+                new_sheet.append(tuple(i.value for i in rows_it[index]))
+                new_sheet.append(tuple(i.value for i in rows_en[index]))
+                new_sheet.append(tuple(i.value for i in rows_nl[index]))
+            os.makedirs(
+                os.path.join(os.getcwd(), "inputs/VolumesExcel"),
+                exist_ok=True,
+            )
+            new_file.save(f"outputs/VolumesExcel/Control_{filename}")
+            print(f"Wrote file to outputs/VolumesExcel/Control_{filename}")
+
+
+def create_translated_xlsx(directory_name, localization):
+    """Creates the .xlsx files with a number of easy translation already done
+
+    Args:
+        directory_name (str): Name of the directory with the input .xlsx files
+        localization (str): Localization abbreviation ("nl_NL", "it_IT", "en_GB")
+    """
+    with open("inputs/Translations/DocumentTitles.json") as file:
+        translations = json.load(file)
+    del translations["$schema"]
+
+    directory = os.fsencode(directory_name)
+    for file in os.listdir(directory):
+        if not str(file).count("~$") and str(file).startswith("b'Paesi"):
+            filename = os.fsdecode(file)
+            translate_xlsx(
+                directory_name,
+                filename,
+                localization,
+                translations
+            )
+
+
 if __name__ == "__main__":
-    create_xlsx("inputs/VolumesExcel/en_GB", "en_GB")
-    create_xlsx("inputs/VolumesExcel/it_IT", "it_IT")
-    create_xlsx("inputs/VolumesExcel/nl_NL", "nl_NL")
+    # create_filled_xlsx("inputs/VolumesExcel/en_GB", "en_GB")
+    # create_filled_xlsx("inputs/VolumesExcel/it_IT", "it_IT")
+    # create_filled_xlsx("inputs/VolumesExcel/nl_NL", "nl_NL")
+    create_translated_xlsx("inputs/VolumesExcel/it_IT", "en_GB")
+    """create_xlsx_controle(
+        [
+            "inputs/VolumesExcel/original",
+            "inputs/VolumesExcel/it_IT",
+            "inputs/VolumesExcel/en_GB",
+            "inputs/VolumesExcel/nl_NL",
+        ]
+    )"""
