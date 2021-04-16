e7a25e5d5774bde0cf1d0f4bd53409dbcaa947d9
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-04-16 12:26:56 +0200

    Extracted name filling to own file

---
+++ python/functions/xlsx_fill_in_names.py
@@ -0,0 +1,48 @@
+import os
+import re
+
+from openpyxl import load_workbook
+
+from functions.data_create_name_string import name_string
+
+
+def fill_in_xlsx(directory_name, file_name, individuals, translations, localization):
+    """Fills an individual volume .xlsx file
+
+    Args:
+        directory_name (str): Name of input directory
+        file_name (str): Name of input file
+        individuals (dict): Dictionary of all individuals based on inputs/Individuals.json
+        translations (list): Dictionaries of function and title translations
+        localization (str): Localization abbreviation ("nl_NL", "it_IT", "en_GB")
+    """
+    workbook = load_workbook(f"{directory_name}/{file_name}")
+    for row in workbook[workbook.sheetnames[0]].iter_rows():
+        if row[1].value is not None:
+            date = [row[2].value, row[3].value, row[4].value]
+            line = re.split(r"( |\.|,|\(|\))", row[1].value)
+            for index, word in enumerate(line):
+                if word.startswith("$"):
+                    line[index] = name_string(
+                        individuals[word], date, translations, localization
+                    )
+            line = "".join(line)
+
+            # Clean up string
+            if line[-1] in [" ", ",", "."]:  # Remove final characters
+                line = line[:-1]
+            line = line[0].upper() + line[1:]
+            line = line.replace("( ", "(").replace(" )", ")")
+            if line == " ":
+                line = ""
+            row[1].value = line
+
+    new_directory = directory_name.replace(
+        "VolumesExcel/", "VolumesExcelFilled/Final_"
+    ).replace("inputs", "outputs")
+    os.makedirs(
+        os.path.join(os.getcwd(), new_directory),
+        exist_ok=True,
+    )
+    workbook.save(f"{new_directory}/Final_{file_name}")
+    print(f"File written to {new_directory}/Final_{file_name}")
