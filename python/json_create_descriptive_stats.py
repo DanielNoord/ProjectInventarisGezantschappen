821e5a2cc9aa4a5f9cfab93c5b2246dd7eab8d61
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-05-04 17:43:10 +0200

    Make type_lost (non)-skippable

--- python/json_create_descriptive_stats.py
+++ python/json_create_descriptive_stats.py
@@ -1,12 +1,13 @@
 import json
 
 
-def create_statistics(filename, skip_types):
-    """Print out statistics from from .json file
+def create_type_statistics(filename, type_list, skip):
+    """Print out statistics from from .json file for specified person types
 
     Args:
         filename (str): Name of the input file
-        skip_types (list): Number of person types you might want to skip.
+        type_list (list): Number of person types you might want to skip.
+        skip (bool): Whether type_list should be seen as a skip list or as a list to select
     """
     c_comment = 0
     c_comment_daniel = 0
@@ -25,7 +26,9 @@ def create_statistics(filename, skip_types):
         persons = json.load(file)
     del persons["$schema"]
     for data in persons.values():
-        if data["person_type"] not in skip_types:
+        if (skip and data["person_type"] not in type_list) or (
+            not skip and data["person_type"] in type_list
+        ):
             if data["comment"] != "":
                 c_comment += 1
             if data["comment_daniel"] != "":
@@ -51,7 +54,10 @@ def create_statistics(filename, skip_types):
             if data["titles"] != []:
                 c_titles += 1
     start_string = "    Number of entries with"
-    print(f"For the entries excluding the types {' and '.join(str(i) for i in skip_types)}")
+    if skip:
+        print(f"For the entries excluding the types {' and '.join(str(i) for i in type_list)}")
+    else:
+        print(f"For the entries with the types {' and '.join(str(i) for i in type_list)}")
     print(f"{start_string} comments: {c_comment}, {c_comment/c_surname:.2%}")
     print(f"{start_string} 'Daniel' comment: {c_comment_daniel}, {c_comment_daniel/c_surname:.2%}")
     print(f"{start_string} birth dates: {c_date_of_birth}, {c_date_of_birth/c_surname:.2%}")
@@ -66,5 +72,32 @@ def create_statistics(filename, skip_types):
     print(f"{start_string} titles: {c_titles}, {c_titles/c_surname:.2%}")
 
 
+def create_total_statistics(filename):
+    """Print out statistics from from .json file for total file
+
+    Args:
+        filename (str): Name of the input file
+    """
+    c_types = [0, 0, 0, 0, 0, 0, 0]
+
+    with open(filename) as file:
+        persons = json.load(file)
+    del persons["$schema"]
+    for data in persons.values():
+        c_types[data["person_type"]] += 1
+
+    start_string = "    Number of entries with"
+    total_count = len(persons.keys())
+    print("For the full database:")
+    print(f"{start_string} type 0: {c_types[0]}, {c_types[0]/total_count:.2%}")
+    print(f"{start_string} type 1: {c_types[1]}, {c_types[1]/total_count:.2%}")
+    print(f"{start_string} type 2: {c_types[2]}, {c_types[2]/total_count:.2%}")
+    print(f"{start_string} type 3: {c_types[3]}, {c_types[3]/total_count:.2%}")
+    print(f"{start_string} type 4: {c_types[4]}, {c_types[4]/total_count:.2%}")
+    print(f"{start_string} type 5: {c_types[5]}, {c_types[5]/total_count:.2%}")
+    print(f"{start_string} type 6: {c_types[6]}, {c_types[6]/total_count:.2%}")
+
+
 if __name__ == "__main__":
-    create_statistics("inputs/Individuals.json", [])
+    create_type_statistics("inputs/Individuals.json", [0, 1, 2, 3, 4, 5, 6], False)
+    create_total_statistics("inputs/Individuals.json")
