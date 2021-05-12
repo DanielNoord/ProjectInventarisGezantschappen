23b404dd4a9b3b89ff46f4336c53a0e1ab9f3b0d
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-05-12 16:13:32 +0200

    Check document numbers

--- python/functions/xlsx_sanitize.py
+++ python/functions/xlsx_sanitize.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from openpyxl import load_workbook
 
@@ -10,13 +11,32 @@ def sanitize_xlsx(directory_name, file_name):
         directory_name (str): Directory of .xlsx file
         file_name (str): Name of .xlsx file
     """
+    last_line = 0
     workbook = load_workbook(f"{directory_name}/{file_name}")
     for row in workbook[workbook.sheetnames[0]].iter_rows():
         for index, cell in enumerate(row):
             if cell.value is not None:
-                line = cell.value
+                # # Check document number for inconsistensies
+                # if index == 0:
+                #     if not cell.value.startswith("ms"):
+                #         raise Exception(f"Incorrect document number: {cell.value}")
+                #     document_num = re.match(r"(.*_)+(.*)", cell.value).groups()[-1]
+                #     if (
+                #         document_num == str(last_line + 1)
+                #         or document_num == f"{last_line}bis"
+                #         or document_num in ["0", "1"]
+                #         or "-" in document_num
+                #     ):
+                #         if "-" in document_num:
+                #             last_line = 0
+                #         elif not document_num.endswith("bis"):
+                #             last_line = int(document_num)
+                #     else:
+                #         print(cell.value)
+                #         last_line = int(document_num)
 
                 # Clean up string
+                line = cell.value
                 if line == " ":
                     line = ""
                 elif isinstance(line, str):
