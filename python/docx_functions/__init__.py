2de5fe2690981864f428977d4e2854daad36fa9c
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-09-09 21:07:04 +0200

    Code: Update docx_functions

--- python/docx_functions/__init__.py
+++ python/docx_functions/__init__.py
@@ -1,7 +1,10 @@
 from docx_functions.docx_make import (
-    list_to_be_translated,
+    database,
     list_of_translated_data,
     list_of_translated_data_with_style,
+    list_to_be_translated,
     list_with_style,
-    database,
 )
+from docx_functions.parse_function_string import parse_function
+from docx_functions.parse_person_string import parse_person
+from docx_functions.parse_title_string import parse_title
---
+++ python/docx_functions/parse_function_string.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import re
+from typing import Optional
+
+
+def parse_function(input_line: str) -> list[tuple[str, Optional[str]]]:
+    """Parses a function string
+
+    Args:
+        input_line (str): Line to be parsed
+
+    Returns:
+        list[tuple[str, Optional[str]]]: List of tuples
+            Each list contains two strings which are title of function and timperiod of function
+    """
+    pattern = re.compile(r"\w.+?(?=\||$)", re.DOTALL)
+    functions = re.findall(pattern, input_line)
+    for i, func in enumerate(functions):
+        pattern2 = re.compile(r"(.*) \((.+)\)")
+        if mat := re.match(pattern2, func):
+            functions[i] = mat.groups()
+        else:
+            functions[i] = [func, None]
+    return functions
