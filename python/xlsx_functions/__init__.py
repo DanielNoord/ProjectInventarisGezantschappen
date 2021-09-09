9f11d076b09095a639eb6a8d7640abae7b30903b
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-09-09 21:12:33 +0200

    Code: New xlsx_functions module

---
+++ python/xlsx_functions/__init__.py
@@ -0,0 +1,4 @@
+from xlsx_functions.fill_in_names import fill_in_xlsx
+from xlsx_functions.parse import parse_dossier, parse_file, parse_volume
+from xlsx_functions.sanitize import sanitize_xlsx
+from xlsx_functions.translate import translate_xlsx
---
+++ python/xlsx_functions/fill_in_names.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os
+import re
+
+from data_parsing import name_string
+from openpyxl import load_workbook
+
+
+def fill_in_xlsx(
+    directory_name: str,
+    file_name: str,
+    individuals: dict,
+    translations: tuple[dict, dict, dict],
+    localization: str,
+) -> None:
+    """Fills an individual volume .xlsx file
+
+    Args:
+        directory_name (str): Name of input directory
+        file_name (str): Name of input file
+        individuals (dict): Dictionary of all individuals based on inputs/Individuals.json
+        translations (tuple[dict, dict, dict]): Dictionaries of function, title and places translations
+        localization (str): Localization abbreviation ("nl_NL", "it_IT", "en_GB")
+    """
+    workbook = load_workbook(f"{directory_name}/{file_name}")
+    for row in workbook[workbook.sheetnames[0]].iter_rows():
+        if row[1].value is not None:
+            date = [row[2].value, row[3].value, row[4].value]
+            line = re.split(r"( |\.|,|\(|\))", row[1].value)
+            for index, word in enumerate(line):
+                if word.startswith("$"):
+                    line[index] = name_string(individuals[word], date, translations, localization)
+            line = "".join(line)
+
+            # Clean up string
+            line = line[0].upper() + line[1:]
+            line = line.replace("{", "(").replace("}", ")")
+            row[1].value = line
+
+    new_directory = (
+        directory_name.replace("VolumesExcelSanitized/", "VolumesExcelFinal/")
+        .replace("VolumesExcelTranslated/", "VolumesExcelFinal/")
+        .replace("inputs", "outputs")
+    )
+    os.makedirs(
+        os.path.join(os.getcwd(), new_directory),
+        exist_ok=True,
+    )
+    workbook.save(f"{new_directory}/{file_name}")
+    print(f"File written to {new_directory}/{file_name}")
