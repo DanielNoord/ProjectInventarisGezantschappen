08c63f0aa2e4a5665d9cc07e725565d6487c2364
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-10-06 00:09:11 +0200

    Code: Add docstrings

--- python/typing_classes.py
+++ python/typing_classes.py
@@ -2,6 +2,8 @@ from typing import NamedTuple, Optional
 
 
 class VolData(NamedTuple):
+    """Data about a volume"""
+
     num: str
     title_it: str
     title_en: str
@@ -10,6 +12,8 @@ class VolData(NamedTuple):
 
 
 class DateData(NamedTuple):
+    """Dates formatted in the different languages (month names)"""
+
     date1_it: Optional[str]
     date2_it: Optional[str]
     date1_en: Optional[str]
