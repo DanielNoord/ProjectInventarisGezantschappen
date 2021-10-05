4e602878ff9c2b9eef0aa830ec34d2eacd722c0f
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-10-05 23:45:32 +0200

    Code: Improve volume and dossier parsing

--- python/xlsx_functions/parse.py
+++ python/xlsx_functions/parse.py
@@ -2,30 +2,27 @@
 
 import re
 from datetime import date
+from typing import Union
 from warnings import warn
 
 from date_functions import check_date_earlier, check_date_later
 from openpyxl.cell.cell import Cell
 from openpyxl.worksheet.worksheet import Worksheet
+from typing_classes import VolData
 
 
-def parse_volume(vol: tuple[Cell, ...]) -> tuple[str, str, str]:
-    """Parses volumes. Note that current .xlsx files do not contain volume description.
-
-    Args:
-        vol: Volume to be parsed
-
-    Returns:
-        str: Number of the volume
-        str: Title of the volume
-        str: Date of the volume in the format xxxx-xx-xx/xxxx-xx-xx
-    """
+def parse_volume(vol: tuple[Cell, ...]) -> VolData:
+    """Parses volumes. Note that current .xlsx files do not contain volume description"""
     if not isinstance(vol[0].value, (str)):
-        raise ValueError("Volume number should be a string")
+        raise ValueError(f"Volume number should be a string, check {vol[0].value}")
     vol_num = re.findall(r"ms(.*?)_.*", vol[0].value)[0]
-    vol_title = f"Questo è il volume {vol_num}"
-    vol_date = "2020-01-01/2020-12-31"
-    return vol_num, vol_title, vol_date
+    # TODO: Fix titles of volumes
+    vol_title_it = f"Questo è il titolo del volume {vol_num}"
+    vol_title_en = f"This is the title of volume {vol_num}"
+    vol_title_nl = f"Dit is de titel van volume {vol_num}"
+    # TODO: Fix dates of volumes
+    vol_date = "1800-01-01/2021-12-31"
+    return VolData(vol_num, vol_title_it, vol_title_en, vol_title_nl, vol_date)
 
 
 def parse_dossier(  # pylint: disable=too-many-branches
@@ -33,31 +30,20 @@ def parse_dossier(  # pylint: disable=too-many-branches
     dos_number: str,
     vol_num: str,
     start_cell: Cell,
-) -> tuple[str, str, str]:
-    """Parses a dossier from a .xlsx sheet
-
-    Args:
-        sheet (Worksheet): Sheet with data
-        dos_number (str): Number of the dossier
-        vol_num (str): Number of the volume
-        start_cell (Cell): Starting cell of the dossier
-
-    Returns:
-        str: Number of the dossier
-        str: Title of the dossier
-        str: Date of the dossier in the format xxxx-xx-xx/xxxx-xx-xx
-    """
-    ## Find highest page number in dossier
-    for row_num in range(sheet.max_row, 1, -1):
-        if i := sheet["A"][row_num - 1].value:
-            if i.startswith(f"ms{vol_num}_{dos_number}"):
-                dos_pages_match = re.search(r"ms.*?_.*?_(.*)", i)
-                if dos_pages_match:
-                    dos_pages = dos_pages_match.groups()[0]
-                    break
-                raise ValueError(
-                    f"Can't parse the dossier pages of {dos_number} in {vol_num}"
-                )
+) -> tuple[str, str]:
+    """Parses a dossier from a .xlsx sheet"""
+    # No longer used, but kept for later
+    # ## Find highest page number in dossier
+    # for row_num in range(sheet.max_row, 1, -1):
+    #     if i := sheet["A"][row_num - 1].value:
+    #         if i.startswith(f"ms{vol_num}_{dos_number}"):
+    #             dos_pages_match = re.search(r"ms.*?_.*?_(.*)", i)
+    #             if dos_pages_match:
+    #                 dos_pages = dos_pages_match.groups()[0]
+    #                 break
+    #             raise ValueError(
+    #                 f"Can't parse the dossier pages of {dos_number} in {vol_num}"
+    #             )
 
     ## Parse title from dossier title row
     if not isinstance(start_cell.value, str):
@@ -65,14 +51,23 @@ def parse_dossier(  # pylint: disable=too-many-branches
             f"Number Cell of dossier should be a string. Incorrect for:\n{start_cell.value}"
         )
     if start_cell.value.endswith("_0") and start_cell.row:
-        dos_title = sheet["B"][int(start_cell.row) - 1].value
+        dos_title = str(sheet["B"][int(start_cell.row) - 1].value)
     else:
         dos_title = "Missing dossier title"
         warn(f"Vol: {vol_num} Dos: {dos_number} is missing a dossier title")
 
     ## Find earliest and latest data
-    early_date: tuple[str, str, str] = ("2020", "12", "31")
-    late_date: tuple[str, str, str] = ("0", "0", "0")
+    ## TODO: This can probably be improved performance-wise
+    early_date: tuple[
+        Union[str, int, None], Union[str, int, None], Union[str, int, None]
+    ] = (
+        "2020",
+        "12",
+        "31",
+    )
+    late_date: tuple[
+        Union[str, int, None], Union[str, int, None], Union[str, int, None]
+    ] = ("0", "0", "0")
 
     # Check if any of dates is "better"
     for row in sheet.iter_rows():
@@ -81,7 +76,7 @@ def parse_dossier(  # pylint: disable=too-many-branches
             or not isinstance(row[0], Cell)
             or isinstance(row[0].value, date)
         ):
-            raise ValueError("Unrecognizable row type in {vol_num}")
+            raise ValueError(f"Unrecognizable row type in {vol_num}")
         if row[0].value:
             if not isinstance(row[0].value, str):
                 raise ValueError(
@@ -92,21 +87,29 @@ def parse_dossier(  # pylint: disable=too-many-branches
                 late_date = check_date_later(late_date, row)
 
     # Check for no changes and sanitization
-    early_date_final: tuple[str, ...] = early_date
-    late_date_final: tuple[str, ...] = late_date
-    if early_date == ("2020", "12", "31"):
+    early_date_final: tuple[str, ...] = (
+        str(early_date[0]),
+        str(early_date[1]),
+        str(early_date[2]),
+    )
+    late_date_final: tuple[str, ...] = (
+        str(late_date[0]),
+        str(late_date[1]),
+        str(late_date[2]),
+    )
+    if early_date_final == ("2020", "12", "31"):
         early_date_final = ()
-    if late_date == ("0", "0", "0"):
+    elif early_date_final[1] == "None" and early_date_final[2] != "None":
+        early_date_final = (early_date_final[0],)
+    if late_date_final == ("0", "0", "0"):
         late_date_final = ()
-    if early_date[1] == "None" and early_date[2] != "None":
-        early_date_final = tuple(early_date[0])
-    if late_date[1] == "None" and late_date[2] != "None":
-        late_date_final = tuple(late_date[0])
+    elif late_date_final[1] == "None" and late_date_final[2] != "None":
+        late_date_final = (late_date_final[0],)
     early_date_final = tuple(str(i).zfill(2) for i in early_date_final if i != "None")
     late_date_final = tuple(str(i).zfill(2) for i in late_date_final if i != "None")
-    dos_data = "/".join(["-".join(early_date_final), "-".join(late_date_final)])
+    dos_date = "/".join(["-".join(early_date_final), "-".join(late_date_final)])
 
-    return dos_pages, dos_title, dos_data
+    return dos_title, dos_date
 
 
 def parse_file(row: tuple[Cell, ...]) -> tuple[str, str, str, str]:
