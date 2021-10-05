ecd4114e33444233907b30d23f7e053ab9573b6d
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-10-05 23:46:59 +0200

    Code: Make unitdate create elements for all languages

--- python/xml_functions/unitdate.py
+++ python/xml_functions/unitdate.py
@@ -3,37 +3,55 @@
 from typing import Optional
 
 from lxml import etree
+from typing_classes import DateData
 
 
 def add_unitdate(
-    parent_element: etree._Element,
-    datestring: str,
-    date1: Optional[str],
-    date2: Optional[str],
-    element_type: str,
+    parent_element: etree._Element, datestring: str, date: DateData
 ) -> None:
-    """Creates and appends an unitdate element
-
-    Args:
-        parent_element (etree._Element): The element to attacht the unitdate element to
-        datestring (str): The full date in standard format xxxx-xx-xx/xxxx-xx-xx
-        date1 (str): The begin date in text format (i.e., 1 januari 2021)
-        date2 (str): The end date in text format (i.e., 2 januari 2021)
-        element_type (str): The type of date (i.e., file, volume or function/job)
-    """
+    """Creates and appends an unitdate element"""
     if not datestring:
-        return
-    udate_element = etree.SubElement(
-        parent_element, "unitdate", calendar="gregorian", era="ce", normal=datestring
+        raise ValueError(f"No datestring provided for {parent_element}")
+
+    # Add Italian unitdate
+    udate_element_it = etree.SubElement(
+        parent_element,
+        "unitdate",
+        {"calendar": "gregorian", "era": "ce", "normal": datestring, "lang": "it"},
     )
-    if element_type:
-        if date1:
-            if date2:
-                udate_element.text = f"{date1} tot en met {date2}"
-            else:
-                udate_element.text = f"{date1}"
-        elif date2:
-            udate_element.text = f"Tot en met {date2}"
-            raise Exception(
-                "This element only has an end date. Check if this is correct!"
-            )
+    if date.date1_it:
+        if date.date2_it:
+            udate_element_it.text = f"{date.date1_it} al {date.date2_it}"
+        else:
+            udate_element_it.text = f"{date.date1_it}"
+    elif date.date2_it:
+        udate_element_it.text = f"Al {date.date2_it}"
+        raise ValueError("This element only has an end date. Check if this is correct!")
+
+    # Add English unitdate
+    udate_element_en = etree.SubElement(
+        parent_element,
+        "unitdate",
+        {"calendar": "gregorian", "era": "ce", "normal": datestring, "lang": "en"},
+    )
+    if date.date1_en:
+        if date.date2_en:
+            udate_element_en.text = f"{date.date1_en} till {date.date2_en}"
+        else:
+            udate_element_en.text = f"{date.date1_en}"
+    elif date.date2_en:
+        udate_element_en.text = f"Till {date.date2_en}"
+
+    # Add Dutch unitdate
+    udate_element_nl = etree.SubElement(
+        parent_element,
+        "unitdate",
+        {"calendar": "gregorian", "era": "ce", "normal": datestring, "lang": "nl"},
+    )
+    if date.date1_nl:
+        if date.date2_nl:
+            udate_element_nl.text = f"{date.date1_nl} tot en met {date.date2_nl}"
+        else:
+            udate_element_nl.text = f"{date.date1_nl}"
+    elif date.date2_nl:
+        udate_element_nl.text = f"Tot en met {date.date2_nl}"
