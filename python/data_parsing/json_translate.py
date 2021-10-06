1c87036689590b2e85b8202e70996cb15e9755c8
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-10-06 11:05:35 +0200

    Code: Create ``initialize_translation_database_for_xml``

--- python/data_parsing/json_translate.py
+++ python/data_parsing/json_translate.py
@@ -2,7 +2,11 @@
 
 import json
 
-from typing_utils import TranslationDictCleaned, TranslationDictCleanedTitles
+from typing_utils import (
+    TranslationDictCleaned,
+    TranslationDictCleanedTitles,
+    Translations,
+)
 
 
 def initialize_translation_database() -> tuple[
@@ -33,3 +37,24 @@ def initialize_translation_database() -> tuple[
     del placenames["$schema"]
 
     return (titles, functions, placenames)
+
+
+def initialize_translation_database_for_xml() -> Translations:
+    """Reads the files containing all translations of titles and functions"""
+    with open("inputs/Translations/Titles.json", encoding="utf-8") as file:
+        titles = json.load(file)
+    del titles["$schema"]
+
+    with open("inputs/Translations/Functions.json", encoding="utf-8") as file:
+        functions = json.load(file)
+    del functions["$schema"]
+
+    with open("inputs/Translations/Placenames.json", encoding="utf-8") as file:
+        placenames = json.load(file)
+    del placenames["$schema"]
+
+    with open("inputs/Translations/DocumentTitles.json", encoding="utf-8") as file:
+        document_titles = json.load(file)
+    del document_titles["$schema"]
+
+    return Translations(document_titles, functions, placenames, titles)
