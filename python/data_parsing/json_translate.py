415dd3329262eeb8d72fd85bd8fb583852709960
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-10-06 20:52:19 +0200

    Code: Remove TODO

--- python/data_parsing/json_translate.py
+++ python/data_parsing/json_translate.py
@@ -9,18 +9,7 @@ from typing_utils import Database, TranslationDictCleaned, TranslationDictCleane
 def initialize_translation_database() -> tuple[
     TranslationDictCleanedTitles, TranslationDictCleaned, TranslationDictCleaned
 ]:
-    """Reads the files containing all translations of titles and functions
-
-    Returns:
-        tuple[dict, dict, dict]: A list with the dicts of title, functions and places translations
-
-            For title and functions:
-                Each entry (in "nl_NL") is a dictionary with the keys "nl_NL", "it_IT" and "en_GB"
-                "nl_NL" is double but for ease of translation and reading the input-files in Word
-            For places:
-                Each entry (in "it_T") is a dictionary with the keys "nl_NL" and "en_GB"
-    """
-    # TODO: Update docstring after transforming the databases
+    """Reads the files containing all translations of titles and functions"""
     with open("inputs/Translations/Titles.json", encoding="utf-8") as file:
         titles = json.load(file)
     del titles["$schema"]
