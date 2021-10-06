40302e98e1b110fea77402eb372247ef764a2cde
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-10-06 20:44:38 +0200

    Code: Add Todo

--- python/xml_functions/place_elements.py
+++ python/xml_functions/place_elements.py
@@ -9,14 +9,18 @@ def add_geognames(
     if place == "None":
         return
 
-    data = database.placenames[place]
-    geogname = etree.SubElement(
-        parent_element,
-        "geogname",
-        {"identifier": f"http://www.geonames.org/{data['geonames_id']}"},
-    )
+    # TODO: REMOVE THIS!!!!!
+    try:
+        data = database.placenames[place]
+        geogname = etree.SubElement(
+            parent_element,
+            "geogname",
+            {"identifier": f"http://www.geonames.org/{data['geonames_id']}"},
+        )
 
-    etree.SubElement(geogname, "part", {"lang": "it"}).text = place
-    etree.SubElement(geogname, "part", {"lang": "en"}).text = data["en_GB"]
-    etree.SubElement(geogname, "part", {"lang": "nl"}).text = data["nl_NL"]
+        etree.SubElement(geogname, "part", {"lang": "it"}).text = place
+        etree.SubElement(geogname, "part", {"lang": "en"}).text = data["en_GB"]
+        etree.SubElement(geogname, "part", {"lang": "nl"}).text = data["nl_NL"]
+    except KeyError:
+        print(place)
     # TODO: Add geographiccoordinates element
