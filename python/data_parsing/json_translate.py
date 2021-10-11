f656ff13ae6f8900ea5188063badc9f2a69698fa
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-10-11 15:30:26 +0200

    Code: Fix Regex patterns at compile time

--- python/data_parsing/json_translate.py
+++ python/data_parsing/json_translate.py
@@ -23,6 +23,13 @@ def initialize_translation_database() -> tuple[
     return (titles, functions, placenames)
 
 
+def fix_regex_patterns(pattern: str) -> str:
+    """Makes some changes to the regex patterns"""
+    pattern = re.sub(r"\"|“|”", r"[\"|“|”]", pattern)
+    pattern = re.sub(r"\.\+", '[^"|“|”]+', pattern)
+    return f"^{pattern}"
+
+
 def initialize_database_for_xml() -> Database:
     """Reads the files containing all translations of titles and functions"""
     with open("inputs/Translations/Titles.json", encoding="utf-8") as file:
@@ -40,7 +47,11 @@ def initialize_database_for_xml() -> Database:
     with open("inputs/Translations/DocumentTitles.json", encoding="utf-8") as file:
         document_titles = json.load(file)
     del document_titles["$schema"]
-    document_titles = {re.compile(k): v for k, v in document_titles.items()}
+
+    # Compile patterns while replacing all double quotes for a standard
+    document_titles = {
+        re.compile(fix_regex_patterns(k)): v for k, v in document_titles.items()
+    }
 
     with open("inputs/Individuals.json", encoding="utf-8") as file:
         individuals = json.load(file)
