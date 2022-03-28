6acdd089c693cc371d323637e501d389bbf43034
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-03-28 13:38:06 +0200

    Add ``FileRenamerExcel``

--- python/rename_scans.py
+++ python/rename_scans.py
@@ -1,5 +1,8 @@
 import os
 import re
+from pathlib import Path
+
+from openpyxl import load_workbook
 
 
 # pylint: disable-next=too-few-public-methods
@@ -43,6 +46,114 @@ class FileRenamer:
         print(f"Renamed {self.renamed} files.")
 
 
+# pylint: disable-next=too-few-public-methods
+class FileRenamerExcel:
+    """File renamer based on  names in Excel."""
+
+    def __init__(self, excel_dir: str, scans_dir: str, test: bool) -> None:
+        self.excel_files: list[str] = []
+        """All filenames of the excel files."""
+
+        for file in os.listdir(excel_dir):
+            if file.endswith(".xlsx") and not file.startswith("~$"):
+                self.excel_files.append(f"{excel_dir}/{file}")
+
+        self.excel_files = sorted(self.excel_files)
+
+        self.scans_dir = scans_dir
+        """Location of the scans."""
+
+        self.test_run = test
+        """Whether this is a dry run without renaming any files."""
+
+    def _get_rename_pairs(self, filename: str) -> tuple[str, dict[str, str]]:
+        """Get the rename pairs from a workbook and remove it from the row."""
+        workbook = load_workbook(filename)
+        first_sheet = workbook[workbook.sheetnames[0]]
+
+        # Get the volume name
+        folder = first_sheet["A"][0].value.split("_")[0].upper()
+        assert isinstance(folder, str)
+        assert folder.startswith("MS")
+
+        rename_pairs: dict[str, str] = {}
+        for row in first_sheet.iter_rows():
+            # Only check long enough rows
+            if not len(row) >= 10:
+                continue
+
+            # See if there is a scan name
+            if not (scan_name := row[9].value):
+                continue
+
+            assert isinstance(scan_name, str)
+            scan_name = scan_name.rstrip()
+            if scan_name.count("r") > 1:
+                if self.test_run:
+                    print(f"DEBUG: Double 'r' in {scan_name}")
+                else:
+                    raise ValueError(
+                        f"We can't handle names with two 'r's. See {scan_name}"
+                    )
+            if scan_name in rename_pairs:
+                if self.test_run:
+                    print(f"DEBUG: Double scan name for {scan_name}")
+                else:
+                    raise ValueError(f"Double scan name found: {scan_name}")
+            rename_pairs[scan_name] = f"{row[0].value}r"
+            rename_pairs[scan_name.replace("r", "v")] = f"{row[0].value}v"
+            row[9].value = None
+
+        workbook.save(filename.replace("inputs", "outputs"))
+
+        return folder, rename_pairs
+
+    def _get_files_to_rename(self) -> dict[str, dict[str, str]]:
+        """Iterate over all excel_files and check for renameable scans."""
+        to_rename: dict[str, dict[str, str]] = {}
+        os.makedirs(
+            str(Path(self.excel_files[0]).parent).replace("inputs", "outputs"),
+            exist_ok=True,
+        )
+        for filename in self.excel_files:
+            if not self.test_run:
+                print(f"Checking {filename}.")
+            folder, rename_pairs = self._get_rename_pairs(filename)
+            to_rename[folder] = rename_pairs
+        return to_rename
+
+    def _check_scans_exist(self, rename_pairs: dict[str, dict[str, str]]) -> None:
+        """Check if scans to rename actually exist."""
+        for folder, pairs in rename_pairs.items():
+            if pairs:
+                foldername = f"{self.scans_dir}/{folder}"
+                for old in pairs:
+                    oldname = f"{foldername}/{old}.tif"
+                    if not os.path.exists(oldname):
+                        if self.test_run:
+                            print(f"DEBUG: Can't find scan {old}")
+                        else:
+                            raise ValueError(f"Missing scan for {oldname}")
+
+    def _rename_scans(self, rename_pairs: dict[str, dict[str, str]]) -> None:
+        """Rename scans based on rename pairs."""
+        for folder, pairs in rename_pairs.items():
+            if pairs:
+                foldername = f"{self.scans_dir}/{folder}"
+                for old, new in pairs.items():
+                    oldname = f"{foldername}/{old}.tif"
+                    newname = f"{foldername}/{new}.tif"
+                    print(f"Renamed: {oldname} -> {newname}")
+                    os.rename(oldname, newname)
+
+    def rename_files(self) -> None:
+        """Full loop of renaming scan files"""
+        to_rename = self._get_files_to_rename()
+        self._check_scans_exist(to_rename)
+        if not self.test_run:
+            self._rename_scans(to_rename)
+
+
 if __name__ == "__main__":
     renamer = FileRenamer(
         "/Users/daniel/DocumentenLaptop/MEGA/",
@@ -50,3 +161,11 @@ if __name__ == "__main__":
         "MS278_3_",
     )
     renamer.rename_files()
+
+    # Uncomment to run excel file renamer in test mode.
+    # renamer = FileRenamerExcel(
+    #     "inputs/VolumesExcel/it_IT",
+    #     "/Volumes/Seagate Basic Media/VolumesLegazione",
+    #     test=True,
+    # )
+    # renamer.rename_files()
